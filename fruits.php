837e16b2b5fc11abef1a1061b87c75e7a83c2d67
khushbakht_odilov@mail.ru <khushbakht_odilov@mail.ru>
2023-09-06 17:41:26 +0500

    init

---
+++ fruits.php
@@ -0,0 +1,32 @@
+interface FruitsActions {
+    
+    public function add(string $name, int $price);
+    public function change($name, $price);
+    public function delete($name);
+    public function total();
+    
+}
+
+class Fruits implements FruitsActions {
+
+    public __construct() {
+
+    }
+
+    public function add(string $name, int $price) {
+
+    }
+
+    public function change($name, $price) {
+
+    }
+
+    public function delete($name) {
+
+    }
+
+    public function total() {
+
+    }
+
+}
